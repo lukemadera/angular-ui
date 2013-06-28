5fbd6714f84b7cf5be8e63a8c081ebd6e1bc3d9c
Luke Madera <luke.madera@gmail.com>
2013-06-28 15:20:29 -0700

    multifileupload and carousel directives

---
+++ modules/directives/carousel/carousel.js
@@ -0,0 +1,425 @@
+/**
+@todo
+- keep slide position on resize (it jumps to start then jumps to end/current when click nav after that)
+- same-height attr seems to break it (everything is 0 height)?
+- vertical center prev/next arrows (either position absolute negative margin hack or flexbox?)
+- remove jQuery dependency (about 5 uses left..)
+- [maybe?] option to automatically make it one slide at a time with overflow hidden, display inline-block, etc. so they just pass in slides and it looks like ui-bootstrap carousel
+
+NOTE: there's already a ui-bootstrap carousel which is similar; I wrote this one (rather than using / modifying the existing one) because:
+- I'm adding in (hammer) swipe functionality options so need to move the template to the compile function to conditionally build the HTML
+- I want it to work without twitter bootstrap (which the existing one didn't seem to)
+
+Takes a parent element with a bunch of children and makes the children horizontally scrollable. The children can be any html; this will use the .wrap function to wrap the children and add previous & next clickable arrows as well as touch swipe support to scroll through content horizontally
+NOTE: you are responsible for using "display:inline-block" or similar to get the content to display side to side rather than vertically as well as keeping them in one line (i.e. "white-space:nowrap;")
+
+@dependencies
+OPTIONAL
+- font-awesome for the next & prev arrows (or you can replace it with whatever you want for the classes 'icon-chevron-left' and 'icon-chevron-right' or by passing in htmlPrev and/or htmlNext attributes of your own custom HTML)
+- angular-hammer.js and hammer.js IF using hammer-swipe attribute (defaults to false so this isn't need by default and will work without it)
+	- https://github.com/randallb/angular-hammer
+	- http://eightmedia.github.io/hammer.js/
+
+//TOC
+0. init
+1. initMaxSlides function
+2. updateAnimateInfo function
+3. scope.$on('lmContentSliderReInit', ..
+	// broadcast this to update if content changes
+4. scope.nav function
+
+
+scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html)
+	// @param {Number} interval The time, in milliseconds, that it will take the carousel to go to the next slide.
+	// @param {Boolean} noTransition Whether to disable transitions on the carousel.
+	// @param {Boolean} noPause Whether to disable pausing on the carousel (by default, the carousel interval pauses on hover)
+	@param {Object} [opts]
+
+attrs
+	@param {String} id Instance id
+	@param {Number} [sameHeight =0] 1 (true) if want to make all elements the same height as the tallest one
+	//@param {Number} [noCenter] 1 (true) to not start with the first one centered in the middle
+	@param {Number} [centerOffset =0] Number of pixels to start from left (otherwise it will default to centering the first element)
+	// @param {Number} [touchShowArrows =0] 1 (true) to show next/prev arrows even on touch
+	@param {Number} [showArrows =1] 1 (true) to show next/prev arrows even on touch
+	@param {String} [htmlPrev] HTML for "previous" (left) arrow (otherwise default styles will be used)
+	@param {String} [htmlNext] HTML for "next" (right) arrow (otherwise default styles will be used)
+	@param {Number|String} [navNumItems =0] Either int of number of slides to nav through on each click OR string of 'width' to calculate number of slides based on total width (will show all new items on nav)
+	@param {Number} [alwaysShowNav =0] 1 if want to NEVER hide arrows (even if all content could fit so there's no need to scroll)	//@todo this is mostly to avoid an issue with centering where there's no nav even though some content is off the screen.. just fix this issue instead??
+	@param {Number} [hammerSwipe =0] 1 to supprt hammer.js swiping to change slides
+	@param {Number} [swipeOverlay =0] 1 to put a transparent div on top of all the slides for swiping (since swipe doesn't work on images). NOTE: this will make all your content un-clickable/actionable!!
+
+
+@usage
+partial / html:
+<div ui-carousel hammer-swipe='1' swipe-overlay='1'>
+	<div ng-repeat='slide in slides' style='display:inline-block; text-align:center; vertical-align:top;'>		<!-- styles are optional and should be moved to a class / stylesheet; this centers things and makes them display side by side -->
+		<!-- custom content here -->
+		<img ng-src="{{slide.image}}" style="margin:auto; width:100%;">		<!-- styles are optional and should be moved to a class / stylehseet; this makes the content/images dynamic full width. Remove width:100%; for them to keep their size and be centered -->
+		<div>
+			<h4>Slide {{$index}}</h4>
+			<p>{{slide.text}}</p>
+		</div>
+		<!-- end: custom content here -->
+	</div>
+</div>
+
+controller / js:
+// $scope.myInterval = 5000;
+var slides = $scope.slides = [];
+$scope.addSlide = function() {
+	var newWidth = 200 + ((slides.length + (25 * slides.length)) % 150);
+	slides.push({
+		image: 'http://placekitten.com/' + newWidth + '/200',
+		text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+		['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+	});
+};
+for (var i=0; i<4; i++) {
+	$scope.addSlide();
+}
+
+//end: EXAMPLE usage
+*/
+
+// 'use strict';
+
+angular.module('ui.directives').directive('uiCarousel', ['uiCarouselResize', '$timeout', function (uiCarouselResize, $timeout) {
+  return {
+		restrict: 'A',
+		transclude: true,
+		scope: {
+			opts: '='
+		},
+
+		compile: function(element, attrs) {
+			var defaults ={'sameHeight':'0', 'centerOffset':'0', 'navNumItems':'1', 'alwaysShowNav':'0', 'showArrows':'1', 'hammerSwipe':'0', 'swipeOverlay':'0'};
+			for(var xx in defaults) {
+				if(attrs[xx] ===undefined) {
+					attrs[xx] =defaults[xx];
+				}
+			}
+			//convert to int
+			var attrsToInt =['sameHeight', 'centerOffset', 'navNumItems', 'alwaysShowNav', 'showArrows', 'hammerSwipe', 'swipeOverlay'];
+			for(var ii=0; ii<attrsToInt.length; ii++) {
+				attrs[attrsToInt[ii]] =parseInt(attrs[attrsToInt[ii]], 10);
+			}
+			
+			//set id
+			if(attrs.id ===undefined) {
+				attrs.id ="uiCarousel"+Math.random().toString(36).substring(7);
+			}
+			var id1 =attrs.id;
+			attrs.ids ={
+				'resizeEvt':id1+"ResizeEvt",
+				'content':id1+"Content"
+			};
+			
+			var htmlPrev, htmlNext;
+			if(attrs.htmlPrev) {
+				htmlPrev =attrs.htmlPrev;
+			}
+			else {
+				htmlPrev ="<div class='ui-carousel-arrow'><div class='ui-carousel-arrow-icon icon-chevron-left'></div></div>";
+			}
+			if(attrs.htmlNext) {
+				htmlNext =attrs.htmlNext;
+			}
+			else {
+				htmlNext ="<div class='ui-carousel-arrow'><div class='ui-carousel-arrow-icon icon-chevron-right'></div></div>";
+			}
+			
+			var html="<div>"+		//MUST have outer div otherwise anything (i.e. the hammer swipe directive) will NOT be compiled since compilation does not happen on the element itself!!
+				"<div class='ui-carousel-cont' ";
+				if(attrs.hammerSwipe) {
+					html+="hm-swipeleft='nav(\"next\", {})' hm-swiperight='nav(\"prev\", {})' hm-options='{swipe_velocity: 0.2}' ";
+				}
+				html+=">";
+				if(attrs.showArrows) {
+					html+="<div ng-show='show.prev' class='ui-carousel-prev' ng-click='nav(\"prev\", {})'><div class='ui-carousel-arrow-outer'>"+htmlPrev+"</div></div>";
+				}
+				if(attrs.swipeOverlay) {
+					html+="<div class='ui-carousel-content-swipe'></div>";
+				}
+				html+="<div id='"+attrs.ids.content+"' class='ui-carousel-content' style='width:{{styles.contentWidth}}px;' ng-transclude></div>";
+				if(attrs.showArrows) {
+					html+="<div ng-show='show.next' class='ui-carousel-next' ng-click='nav(\"next\", {})'><div class='ui-carousel-arrow-outer'>"+htmlNext+"</div></div>";
+				}
+				html+="</div>";
+			html+="</div>";
+			
+			element.replaceWith(html);
+			
+			return function(scope, element, attrs) {
+				//0. init
+				scope.show ={
+					prev: true,
+					next: true
+				};
+				scope.styles ={
+					contentWidth: 0
+				};
+				
+				var curSlide =0;
+				var maxSlides;
+				var page =0;		//if navNumItems is greater than 1, this will store the current page / set of slides we're on (otherwise page will be the same as curSlide
+				var maxPages;		//this stores total pages (where pages are a collection of slides equal to navNumSlides - typically the amount of slides that are visible at the same time). If navNumItems is 1, this will be the same as maxSlides
+				var animateInfo, navNumItems =attrs.navNumItems, alwaysShowNav =false;
+				if(attrs.alwaysShowNav) {
+					alwaysShowNav =true;
+				}
+				
+				if(attrs.navNumItems !==undefined && attrs.navNumItems !='width') {
+					navNumItems =attrs.navNumItems;
+				}
+				if(attrs.alwaysShowNav !==undefined) {
+					alwaysShowNav =attrs.alwaysShowNav;
+				}
+				
+				function init(params) {
+					initMaxSlides({'attempt':1});
+					
+					uiCarouselResize.addCallback(attrs.ids.resizeEvt, {'evtName':attrs.ids.resizeEvt, 'args':[]}, {});
+				}
+				
+				scope.$on(attrs.ids.resizeEvt, function(evt, params) {
+					//console.log('resize');
+					updateAnimateInfo({});
+					//call again after timeout just in case content is changing and not done/resized yet..
+					$timeout(function() {
+						updateAnimateInfo({});
+					}, 1000);
+				});
+				
+				//1.
+				/*
+				@param params
+					attempt =int of which attempt (sometimes have loading/timing issue; so set timeout and try again for an attempt or two to see if get some data..)
+				*/
+				function initMaxSlides(params) {
+					var ele =document.getElementById(attrs.ids.content);
+					//maxSlides =ele.children().length-1;		//-1 is because angular inserts an extra "comment" element that I can't seem to filter out with a "div" tag..
+					// maxSlides =$(ele).children().length;
+					maxSlides =angular.element(ele).children().length;
+					// console.log("maxSlides: "+maxSlides);
+					if(maxSlides <1 && params.attempt <3) {		//try again
+						params.attempt++;
+						//setTimeout(function() {
+						$timeout(function() {
+							initMaxSlides(params);
+						}, 200);
+					}
+					else {
+						updateAnimateInfo({});
+					
+						scope.nav('first', {});		//init
+					}
+				}
+				
+				//2.
+				function updateAnimateInfo(params) {
+					var ele =document.getElementById(attrs.ids.content);
+					animateInfo ={
+						'totWidth':$(ele).parent().outerWidth(true)
+						// 'totWidth':angular.element(ele).parent().outerWidth(true)		//no outerWidth function without jQuery
+						// 'width':$(ele).children(":first-child").outerWidth(true),
+					};
+					animateInfo.width =animateInfo.totWidth;		//set each child to width of parent (to only show one at a time)
+					$(ele).children().each(function() {
+						$(this).width(animateInfo.width);
+					});
+						
+					// maxSlides =$(ele).children().length;
+					maxSlides =angular.element(ele).children().length;
+					
+					// scope.styles.contentWidth =animateInfo.totWidth;		//do NOT set it to parent; needs to be full width of ALL slides side by side
+					scope.styles.contentWidth =animateInfo.width*maxSlides;
+					console.log('animateInfo: '+JSON.stringify(animateInfo)+' maxSlides: '+maxSlides);
+					
+					//if num slides is calculated dynamically, do it now
+					if(attrs.navNumItems !==undefined && attrs.navNumItems =='width') {
+						navNumItems =Math.floor(animateInfo.totWidth /animateInfo.width);
+					}
+					
+					//makes them all the same height..
+					if(attrs.sameHeight) {
+						var maxHeight =0;
+						$(ele).children().each(function() {
+							var curHeight =$(this).height();
+							if(curHeight >maxHeight) {
+								maxHeight =curHeight;
+							}
+						});
+						$(ele).children().each(function() {
+							$(this).height(maxHeight);
+						});
+					}
+					//end: make them all same height
+					
+					if(attrs.centerOffset) {
+						animateInfo.centerOffset =attrs.centerOffset*1;
+					}
+					else {		//center
+						animateInfo.centerOffset =animateInfo.totWidth/2 -animateInfo.width/2;		//holds the position of the first slide margin-left when it's centered
+					}
+					
+					//console.log(animateInfo.totWidth+" "+animateInfo.width*maxSlides);
+					//hide nav arrows if content is less than total width; otherwise show them
+					if(!alwaysShowNav && animateInfo.totWidth >animateInfo.width*maxSlides) {		//wider than content = no arrows
+						scope.show.prev =false;
+						scope.show.next =false;
+						//nav to 0 to ensure all content is visible
+						//scope.nav('first', {});
+						scope.nav('all', {});
+					}
+					else {
+						if(attrs.showArrows) {
+							scope.show.prev =true;
+							scope.show.next =true;
+						}
+					}
+				}
+				
+				//3. $scope.$on('lmContentSliderReInit', ..
+				/*
+				@param args
+					nav =mixed; string of 'prev', 'next', 'first', 'last' OR int of slide to go to
+				*/
+				scope.$on('lmContentSliderReInit', function(evt, args) {
+					updateAnimateInfo({});
+					var ppTemp ={};
+					var to =curSlide;
+					if(args.nav) {
+						to =args.nav;
+					}
+					scope.nav(to, ppTemp);
+				});
+				
+				//4.
+				/*
+				@param to =mixed; string of 'prev', 'next', 'first', 'last' 'all' (shows all content - this assumes total width is larger than all slides together) OR int of slide to go to
+				*/
+				scope.nav =function(to, params) {
+					var ele =document.getElementById(attrs.ids.content);
+					var marginLeft;
+					//console.log("nav: "+to);
+					if(to =='all') {
+						marginLeft =Math.floor((animateInfo.totWidth - animateInfo.width*maxSlides) /2);
+					}
+					else {
+						if(to =='prev') {
+							if(curSlide >0) {
+								//curSlide--;
+								curSlide =curSlide -navNumItems;
+								if(curSlide <0) {
+									curSlide =0;
+								}
+							}
+						}
+						else if(to =='next') {
+							if(curSlide <(maxSlides-navNumItems)) {
+							//if(curSlide <(maxSlides-1)) {
+								//curSlide++;
+								curSlide =curSlide +navNumItems;
+								if(curSlide >=(maxSlides-1)) {
+									curSlide =(maxSlides-1);
+								}
+							}
+						}
+						else if(to =='first') {
+							curSlide =0;
+						}
+						else if(to =='last') {
+							//curSlide =maxSlides-1;
+							curSlide =maxSlides -navNumItems;
+						}
+						else {		//must be an int of which slide to go to
+							curSlide =to;
+						}
+						marginLeft =-1*(+curSlide*animateInfo.width) +animateInfo.centerOffset;
+					}
+					// $(ele).css({'margin-left':marginLeft+'px'});
+					angular.element(ele).css({'margin-left':marginLeft+'px'});
+				};
+				
+				init({});
+			};
+		}
+		
+		// controller: function($scope, $element, $attrs) {
+		// }
+	};
+}])
+.factory('uiCarouselResize', ['$rootScope', function($rootScope){
+var inst ={
+
+	callbacks: {},		//1D array of function callback info ({'evtName' =string of what event name to broadcast, 'args':[]}) to call on each resize
+	timeout: false,
+
+	//0.
+	/*
+	@param params
+		timeout =int of milliseconds to wait between calling resize (for performance to avoid firing every millisecond)
+	*/
+	init: function(params)
+	{
+		var thisObj =this;
+		var defaults ={'timeout':500};
+		params =angular.extend(defaults, params);
+		$(window).resize(function(){
+			if(!thisObj.timeout) {
+				thisObj.timeout =setTimeout(function() {
+					thisObj.resize({});
+					clearTimeout(thisObj.timeout);
+					thisObj.timeout =false;		//reset
+				}, params.timeout);
+			}
+		});
+	},
+	
+	//0.5.
+	destroy: function(params)
+	{
+	},
+
+	//1.
+	resize: function(params)
+	{
+		var thisObj =this;
+		for(var xx in this.callbacks)
+		{
+			// console.log('carousel resize callback: '+xx);
+			if(!$rootScope.$$phase) {		//if not already in apply / in Angular world
+				$rootScope.$apply(function() {
+					$rootScope.$broadcast(thisObj.callbacks[xx].evtName, thisObj.callbacks[xx].args);
+				});
+			}
+			else {
+				$rootScope.$broadcast(thisObj.callbacks[xx].evtName, thisObj.callbacks[xx].args);
+			}
+		}
+	},
+
+	//2.
+	/*
+	@param fxnId =string of associative array key/instance id to use (need this for removing callback later)
+	@param fxnInfo =//1D array of function callback info ({'evtName' =string of what event name to broadcast, 'args':[]}) to call on each resize
+	@param params
+	*/
+	addCallback: function(fxnId, fxnInfo, params)
+	{
+		this.callbacks[fxnId] =fxnInfo;
+	},
+
+	//2.5.
+	removeCallback: function(fxnId, params)
+	{
+		if(this.callbacks[fxnId] && this.callbacks[fxnId] !==undefined)
+			delete this.callbacks[fxnId];
+	}
+
+};
+inst.init();
+return inst;
+}])
+;
---
+++ modules/directives/multifileupload/multiFileUpload.js
@@ -0,0 +1,261 @@
+/**
+Uses a $scope.$on('uiMultiFileUploadGo', {instId:xxx, files:[{file:xx}, {file:yy}]}) to initiate uploading one or more files and displays progress (bar). The server response is passed back via a callback function.
+
+//TOC
+1. init
+2. $scope.$on('uiMultiFileUploadGo',..
+4. $scope.uploadFile =function
+5. function uploadProgress
+6. function uploadComplete
+6.25. function ajaxUploadComplete
+6.5. function afterComplete
+7. function uploadFailed
+8. function uploadCanceled
+
+
+scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html)
+@param {Object} opts
+	@param {String} instId Unique identifer for this instance - used for triggering the $scope.$on to initiate the upload
+	// @param {Array} files Array of files to upload	//UPDATE: these will be passed in on each $scope.$on
+	@param {String} uploadPath Path to upload file to (backend script)
+	@param {Object} [serverParamNames] Form names to submit (so can interact with any server).
+		@param {String} [files ='files'] The key to use for passing back the files. This will be an array of files.
+	@param {Object} [otherData] Key pairs of additional data to send to backend with the request (i.e. {type: 'image'})
+@param {Function} uploadComplete Function to call after successful upload (all data from server will be passed back as is
+
+attrs
+
+
+@usage:
+partial / html:
+<div ui-multi-file-upload opts='opts' upload-complete='uploadComplete' ></div>
+
+controller / js:
+$scope.opts ={
+	instId: 'yes',
+	// files:[
+		// document.getElementById('file1').value
+	// ],
+	uploadPath: '/api/image/upload'
+};
+
+$scope.uploadComplete =function(data, params) {
+	//do something here
+};
+
+//call this function to start the upload
+function startUpload() {
+	//trigger the upload to start
+	$scope.$broadcast('uiMultiFileUploadGo', {instId: $scope.opts.instId, files:[{file:xx}, {file:yy}]});
+}
+
+//end: EXAMPLE usage
+*/
+
+// 'use strict';
+
+angular.module('ui.directives').directive('uiMultiFileUpload', [function () {
+  return {
+		restrict: 'A',
+		scope: {
+			opts: '=',
+			uploadComplete: '&'
+		},
+
+		compile: function(element, attrs) {
+			if(attrs.id ===undefined) {		//would use scope.opts.instId but don't have access to scope yet..
+				attrs.id ="uiMultiFileUpload"+Math.random().toString(36).substring(7);
+			}
+			var id1 =attrs.id;
+			var ids ={
+				'progress':{
+					'barInner':id1+"ProgressBarInner",
+					'bar':id1+"ProgressBar"
+				}
+			};
+			attrs.ids =ids;		//save for later
+			
+			var html ="<div>"+
+				"<div id='"+attrs.ids.progress.bar+"' class='ui-multi-file-upload-progress-bar'><div id='"+attrs.ids.progress.barInner+"' class='ui-multi-file-upload-progress-bar-inner'>&nbsp;</div></div>"+
+				"<div>{{progressNumber}}</div>"+
+				// "<div>{{fileInfo.name}}</div>"+
+				// "<div>{{fileInfo.size}}</div>"+
+				// "<div>{{fileInfo.type}}</div>"+
+			"</div>";
+			element.replaceWith(html);
+		},
+		
+		controller: function($scope, $element, $attrs) {
+			var defaultOpts ={
+				serverParamNames:{
+					files: 'files'
+				}
+			};
+			$scope.opts =angular.extend(defaultOpts, $scope.opts);
+				
+			/**
+			@toc 1.
+			@method init
+			*/
+			function init(params) {
+				// $scope.uploadFiles({});
+			}
+			
+			/**
+			@toc 2.
+			$scope.$on('uiMultiFileUploadGo',..
+			@param {Object} params
+				@param {String} instId
+				@param {Array} files Array of objects with at least a 'file' key for the files to upload (should be an array of local file names OR file data). Each item is an object of:
+					@param {String|File} file The file to upload
+					// @param {String} fileInputId The string of the input element where the file is (will put it's value)
+			*/
+			$scope.$on('uiMultiFileUploadGo', function(evt, params) {
+				if($scope.opts.instId !==undefined && params.instId !==undefined && $scope.opts.instId ==params.instId) {		//only update if the correct instance
+					if(params.files.length >0) {
+						$scope.uploadFiles({files: params.files});
+					}
+					else {
+						console.log('multiFileUpload: no files selected');
+					}
+				}
+			});
+			
+			/**
+			@toc 4.
+			@method $scope.uploadFiles
+			@param {Object} params
+				@param {Array} files Array of objects with at least a 'file' key for the files to upload (should be an array of local file names OR file data). Each item is an object of:
+					@param {String|File} file The file to upload
+					// @param {String} fileInputId The string of the input element where the file is (will put it's value)
+			*/
+			$scope.uploadFiles =function(params) {
+				angular.element(document.getElementById($attrs.ids.progress.barInner)).css({'width':'0%'});
+				if($attrs.showProgress) {
+					var eleProgressBar =angular.element(document.getElementById($attrs.ids.progress.bar));
+					eleProgressBar.removeClass('complete');
+					eleProgressBar.addClass('loading');
+				}
+				else {
+					//LLoading.show({});		//@todo
+				}
+				
+				var fd = new FormData();
+				//add files data
+				var ii;
+				for(ii =0; ii<params.files.length; ii++) {
+					fd.append($scope.opts.serverParamNames.files+'['+ii+']', params.files[ii].file);
+				}
+				
+				//add other data (if any)
+				if($scope.opts.otherData !==undefined) {
+					var xx;
+					for(xx in $scope.opts.otherData) {
+						fd.append(xx, $scope.opts.otherData[xx]);
+					}
+				}
+				
+				var xhr = new XMLHttpRequest();
+				if($attrs.showProgress) {
+					xhr.upload.addEventListener("progress", uploadProgress, false);
+				}
+				xhr.onload =function(ee){uploadComplete(ee, params); };
+				//xhr.addEventListener("load", uploadComplete, false);
+				xhr.onerror =function(ee){uploadFailed(ee, params); };		//doesn't seem to work..
+				//xhr.addEventListener("error", uploadFailed, false);		//doesn't seem to work..
+				xhr.addEventListener("abort", uploadCanceled, false);
+				xhr.open("POST", $scope.opts.uploadPath);
+				xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+				xhr.onreadystatechange =function(){
+					if(xhr.readyState ==4 && xhr.status !=200)
+					{
+						uploadFailed('', params);
+					}
+				};
+				xhr.send(fd);
+			};
+			
+			/**
+			@toc 5.
+			@method uploadProgress
+			*/
+			function uploadProgress(evt) {
+				if (evt.lengthComputable) {
+					var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+					$scope.progressNumber =percentComplete.toString() + '%';
+					document.getElementById($attrs.ids.progress.barInner).style.width = percentComplete.toString() +'%';
+				}
+				else {
+					$scope.progressNumber = 'unable to compute';
+				}
+			}
+			
+			/**
+			@toc 6.
+			@method uploadComplete
+			@param params
+				callback =array {'evtName':string, 'args':[]}
+				uploadFileSimple =boolean true if no display
+			*/
+			function uploadComplete(evt, params) {
+				/* This event is raised when the server send back a response */
+				//alert(evt.target.responseText);
+				
+				document.getElementById($attrs.ids.progress.barInner).style.width = '100%';
+				
+				var ele1 =angular.element(document.getElementById($attrs.ids.progress.bar));
+				ele1.addClass('complete');
+				
+				$scope.progressNumber ='';
+
+				var data =$.parseJSON(evt.target.responseText);
+				afterComplete(params, data);
+			}
+			
+			/**
+			@toc 6.25.
+			@method ajaxUploadComplete
+			*/
+			function ajaxUploadComplete(params, data) {
+				if(typeof(data) =='string') {
+					data =$.parseJSON(data);
+				}
+				afterComplete(params, data);
+			}
+			
+			/**
+			@toc 6.5.
+			@method afterComplete
+			*/
+			function afterComplete(params, data) {
+				//call callback function passed in (if exists)
+				if($scope.uploadComplete !==undefined && $scope.uploadComplete() !==undefined && typeof($scope.uploadComplete()) =='function') {		//this is an optional scope attr so don't assume it exists
+					$scope.uploadComplete()(data, params);
+				}
+				
+				//ensure back in angular world so events fire now
+				if(!$scope.$$phase) {
+					$scope.$apply();
+				}
+			}
+			
+			/**
+			@toc 7.
+			@method uploadFailed
+			*/
+			function uploadFailed(evt) {
+				alert("There was an error attempting to upload the file. Please try again or try a different file.");
+				//LLoading.close({});
+			}
+
+			/**
+			@toc 8.
+			@method uploadCanceled
+			*/
+			function uploadCanceled(evt) {
+				alert("The upload has been canceled by the user or the browser dropped the connection.");
+				//LLoading.close({});
+			}
+		}
+	};
+}]);
